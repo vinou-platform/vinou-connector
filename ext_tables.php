739a06a85bac2521d34e11fd179b5a7754700733
Christian Händel <c.haendel@division.ag>
2017-07-06 14:42:16 +0200

    [BUGFIX] wrong flexform registration

--- ext_tables.php
+++ ext_tables.php
@@ -6,12 +6,12 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,'Configuration/TypoScript','Vinou Connector');
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'Interfrog.'.$_EXTKEY,
+	$_EXTKEY,
 	'Wines',
 	'Vinou Connector'
 );
 
 // Add Flexform for Plugin Products
-$coupons = str_replace('_','',$_EXTKEY) . '_wines';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$coupons] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($coupons, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_wines.xml');
+$wines = str_replace('_','',$_EXTKEY) . '_wines';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$wines] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($wines, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_wines.xml');
