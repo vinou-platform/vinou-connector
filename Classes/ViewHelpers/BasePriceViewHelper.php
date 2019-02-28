f468383ffeb54e8d5aa266909582d317ea6857fd
Christian Händel <c.haendel@division.ag>
2019-02-28 22:47:56 +0100

    [ADD] base price viewhelper

---
+++ Classes/ViewHelpers/BasePriceViewHelper.php
@@ -0,0 +1,17 @@
+<?php
+namespace Vinou\VinouConnector\ViewHelpers;
+
+use \TYPO3\CMS\Extbase\Utility\DebuggerUtility as Debug;
+
+class BasePriceViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+    /**
+     * @param string $price
+     * @param string $size
+     * @return string
+     */
+	public function render($price, $size) {
+
+		return $price / $size;
+	}
+}
