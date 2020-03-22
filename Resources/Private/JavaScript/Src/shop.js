3f85077564a81f15c2b6acb33ac3f3a81f577818
Christian Händel <christian@vinou.de>
2020-03-22 19:16:06 +0100

    [BUGFIX] add item to basket

--- Resources/Private/JavaScript/Src/shop.js
+++ Resources/Private/JavaScript/Src/shop.js
@@ -276,7 +276,7 @@ var vinouShop = {
 			var addButton = addForms[i].querySelector('.add-basket');
 			addButton.addEventListener('click',function(event) {
 				event.preventDefault();
-				ctrl.submitAddForm(addForms[i]);
+				ctrl.submitAddForm(this.form);
 			});
 		}
 
