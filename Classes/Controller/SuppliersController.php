edff15edbe1b9a825718caeab0d1a25917aeda8b
Mike Zimmer <mike@vinou.de>
2021-07-29 10:19:26 +0200

    [ADD] Supplier sorting functionality

--- Classes/Controller/SuppliersController.php
+++ Classes/Controller/SuppliersController.php
@@ -48,31 +48,68 @@ class SuppliersController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
 		$suppliers = [];
 
 		$wineries = $this->api->getWineriesAll([
-            'filter' => [
-                'id' => $ids['winery']
-            ],
-            'addresses' => 1
-        ]);
-        if ($wineries) {
-            foreach ($wineries as &$winery) {
-                $winery['type'] = 'winery';
-                $suppliers['winery_' . $winery['id']] = $winery;
-            }
-        }
-
-        $merchants = $this->api->getMerchantsAll([
-            'filter' => [
-                'id' => $ids['merchant']
-            ],
+            // 'filter' => [
+            //     'id' => $ids['winery']
+            // ],
             'addresses' => 1
         ]);
 
-        if ($merchants){
-            foreach ($merchants as &$merchant) {
-                $merchant['type'] = 'merchant';
-                $suppliers['merchant_' . $merchant['id']] = $merchant;
-            }
-        }
+		if ($wineries) {
+				foreach ($wineries as &$winery) {
+						$winery['type'] = 'winery';
+						$suppliers['winery_' . $winery['id']] = $winery;
+				}
+		}
+
+		$merchants = $this->api->getMerchantsAll([
+				// 'filter' => [
+				//     'id' => $ids['merchant']
+				// ],
+				'addresses' => 1
+		]);
+
+		if ($merchants){
+				foreach ($merchants as &$merchant) {
+						$merchant['type'] = 'merchant';
+						$suppliers['merchant_' . $merchant['id']] = $merchant;
+				}
+		}
+
+		$filters = [
+			'sortBy' => ['company','city','type','zip'],
+			'sortDirection' => ['ASC','DESC']
+		];
+
+		foreach($filters as $name => $values){
+			$sessionVar = $GLOBALS['TSFE']->fe_user->getKey('ses', 'suppliersList'.ucfirst($name));
+			if(in_array($sessionVar,$values)) $this->settings[$name] = $sessionVar;
+
+			if ($this->request->hasArgument($name)) {
+				$requestVar = $this->request->getArgument($name);
+				if(in_array($requestVar,$values)) {
+					$this->settings[$name] = $requestVar;
+					// echo "setSession:";
+					// echo 'suppliersList'.ucfirst($name);
+					// die;
+					$GLOBALS['TSFE']->fe_user->setKey('ses', 'suppliersList'.ucfirst($name), $this->settings[$name]);
+				}
+			}
+		}
+
+		$this->view->assign('settings', $this->settings);
+
+
+		$sortProperty = strlen($this->settings['sortBy']) > 0 ? $this->settings['sortBy'] : reset($filters['sortBy']);
+		$sortDirection = $this->settings['sortDirection'];
+
+
+		usort($suppliers, function($a, $b) use ($sortProperty, $sortDirection) {
+			if (strcmp($sortDirection, 'ASC') == 0)
+				return $a[$sortProperty] > $b[$sortProperty];
+			else
+				return $a[$sortProperty] < $b[$sortProperty];
+		});
+
 
 		$this->view->assign('suppliers', $suppliers);
 	}
@@ -87,7 +124,7 @@ class SuppliersController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
 
 		if (!$this->request->hasArgument('type'))
 			return;
-		
+
 		$identifier = null;
 		if ($this->request->hasArgument('id'))
 			$identifier = $this->request->getArgument('id');
